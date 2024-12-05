70a4cd254b121b9e5e38e9bbb3204813ba29c85c
AMIT JAIN <mca.amit89@gmail.com>
2024-12-05 18:57:25 +0530

    added the file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ConvertPipe } from './convert.pipe';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,ConvertPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pipes';
+  name!: string;
+  date!: string;
+  amount!: number;
+  height!: number;
+  miles!: number;
+
+  car={
+    make:'Toyota',
+    model:'camery',
+    year:'2000'
+  }
+  
+  onMilesChange(value:string)
+  {
+    this.miles=parseFloat(value);
+  }
+
+  onHeightChange(value:string)
+  {
+      this.height=parseFloat(value);
+  }
+  onNameChange(value:string)
+  {
+    this.name=value;
+  }
+  onDateChange(value:string)
+  {
+    this.date=value;
+  }
+  onAmountChange(value:string)
+  {
+    this.amount=parseFloat(value);
+  }
 }
